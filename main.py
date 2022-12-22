d4e3bd1e4e444a73803ccce12fcab34bfee9444e
Andrew Choi <a.choi@smartsoltech.kr>
2022-12-22 19:43:48 +0900

    Yandex Weather cuted off. No Yandex - No problems XD

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ if os.path.exists(dotenv_path):
 
 token = dotenv.get_key(dotenv_path, 'token')
 token_accu = dotenv.get_key(dotenv_path, 'token_accu')
-token_yandex = dotenv.get_key(dotenv_path, 'token_yandex')
 cities = os.path.dirname(__file__) + '\cities.json'
 
 # получаем код города
@@ -59,64 +58,12 @@ def print_weather(dict_weather, message):
                                            f'{dict_weather["link"]}')
 
 
-# сообщение с погодой от яндекса
-def print_yandex_weather(dict_weather_yandex, message):
-    day = {'night': 'ночью', 'morning': 'утром', 'day': 'днем', 'evening': 'вечером', 'fact': 'сейчас'}
-    bot.send_message(message.from_user.id, f'А яндекс говорит:')
-    for i in dict_weather_yandex.keys():
-        if i != 'link':
-            time_day = day[i]
-            bot.send_message(message.from_user.id, f'Температура {time_day} {dict_weather_yandex[i]["temp"]}'
-                                                   f', на небе {dict_weather_yandex[i]["condition"]}')
-
-    bot.send_message(message.from_user.id, f' А здесь ссылка на подробности '
-                                           f'{dict_weather_yandex["link"]}')
-
 # функция получения координат через библиотеку geopy
 def geo_pos(city: str):
     geolocator = geocoders.Nominatim(user_agent="telebot")
     latitude = str(geolocator.geocode(city).latitude)
     longitude = str(geolocator.geocode(city).longitude)
     return latitude, longitude
-
-# нужно получить погоду от яндекса
-def yandex_weather(latitude, longitude, token_yandex: str):
-    url_yandex = f'https://api.weather.yandex.ru/v2/informers/?lat={latitude}&lon={longitude}&[lang=ru_RU]'
-    yandex_req = req.get(url_yandex, headers={'X-Yandex-API-Key': token_yandex}, verify=False)
-    conditions = {'clear': 'ясно', 'partly-cloudy': 'малооблачно', 'cloudy': 'облачно с прояснениями',
-                  'overcast': 'пасмурно', 'drizzle': 'морось', 'light-rain': 'небольшой дождь',
-                  'rain': 'дождь', 'moderate-rain': 'умеренно сильный', 'heavy-rain': 'сильный дождь',
-                  'continuous-heavy-rain': 'длительный сильный дождь', 'showers': 'ливень',
-                  'wet-snow': 'дождь со снегом', 'light-snow': 'небольшой снег', 'snow': 'снег',
-                  'snow-showers': 'снегопад', 'hail': 'град', 'thunderstorm': 'гроза',
-                  'thunderstorm-with-rain': 'дождь с грозой', 'thunderstorm-with-hail': 'гроза с градом'
-                  }
-    wind_dir = {'nw': 'северо-западное', 'n': 'северное', 'ne': 'северо-восточное', 'e': 'восточное',
-                'se': 'юго-восточное', 's': 'южное', 'sw': 'юго-западное', 'w': 'западное', 'с': 'штиль'}
-
-    yandex_json = json.loads(yandex_req.text)
-    print(f'Ответ от яндекса: {yandex_req.text}')
-    yandex_json['fact']['condition'] = conditions[yandex_json['fact']['condition']]
-    yandex_json['fact']['wind_dir'] = wind_dir[yandex_json['fact']['wind_dir']]
-    for parts in yandex_json['forecast']['parts']:
-        parts['condition'] = conditions[parts['condition']]
-        parts['wind_dir'] = wind_dir[parts['wind_dir']]
-
-    weather = dict()
-    params = ['condition', 'wind_dir', 'pressure_mm', 'humidity']
-    for parts in yandex_json['forecast']['parts']:
-        weather[parts['part_name']] = dict()
-        weather[parts['part_name']]['temp'] = parts['temp_avg']
-        for param in params:
-            weather[parts['part_name']][param] = parts[param]
-
-    weather['fact'] = dict()
-    weather['fact']['temp'] = yandex_json['fact']['temp']
-    for param in params:
-        weather['fact'][param] = yandex_json['fact'][param]
-
-    weather['link'] = yandex_json['info']['url']
-    return weather
 
 # функция сохранения городов:
 def add_city(message):
@@ -160,8 +107,6 @@ def get_text_messages(message):
             code_loc = code_location(latitude, longitude, token_accu)
             you_weather = weather(code_loc, token_accu)
             print_weather(you_weather, message)
-            yandex_weather_x = yandex_weather(latitude, longitude, token_yandex)
-            print_yandex_weather(yandex_weather_x, message)
         else:
             bot.send_message(message.from_user.id, f'О великий и могучий {message.from_user.first_name}!'
                                                    f' Я не знаю Ваш город! Просто напиши:'
@@ -183,8 +128,6 @@ def get_text_messages(message):
             code_loc = code_location(latitude, longitude, token_accu)
             you_weather = weather(code_loc, token_accu)
             print_weather(you_weather, message)
-            yandex_weather_x = yandex_weather(latitude, longitude, token_yandex)
-            print_yandex_weather(yandex_weather_x, message)
         except AttributeError as err:
             bot.send_message(message.from_user.id, f'{message.from_user.first_name}! Не вели казнить,'
                                                    f' вели слово молвить! Я не нашел такого города!'
